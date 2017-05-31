151c07b2eb4d2e18f43799b1cff474f6d3657206
Angela Chieh <angela.chieh@gmail.com>
2017-06-01 00:25:10 +0200

    Add password field

--- app/form.py
+++ app/form.py
@@ -1,8 +1,8 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
     folder = StringField('folder', validators=[DataRequired()])
